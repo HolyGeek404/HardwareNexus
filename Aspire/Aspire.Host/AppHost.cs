09c2b1b6fe54af706a8ac5f4dbba5d153cf93b70
HolyGeek404 <wmatkowski404@proton.me>
2026-06-30 19:17:47 +0200

    Added Website to Aspire

--- Aspire/Aspire.Host/AppHost.cs
+++ Aspire/Aspire.Host/AppHost.cs
@@ -1,3 +1,5 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+builder.AddProject<Projects.Website_Client>("Website");
+
 builder.Build().Run();
--- Website/Website.Client/Program.cs
+++ Website/Website.Client/Program.cs
@@ -1,9 +1,15 @@
+using Aspire.Defaults;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.AddServiceDefaults();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+app.MapDefaultEndpoints();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
