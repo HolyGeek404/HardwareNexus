9a5d5e3fb34e3612c8027a4edb027836d9c56d23
HolyGeek404 <wmatkowski404@proton.me>
2026-06-30 20:58:21 +0200

    added Website_Gateway files

---
+++ Website/Website.Gateway/Program.cs
@@ -0,0 +1,18 @@
+using Website.Gateway.Extensions;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.AddGatewayConfiguration();
+builder.Services.AddGatewayServices(builder.Configuration);
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+app.UseGatewayPipeline();
+await app.UseOcelot();
+app.Run();
