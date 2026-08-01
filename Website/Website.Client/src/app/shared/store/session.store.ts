6e5cb01eac1170fee26276d44638de8ef739208d
HolyGeek404 <wmatkowski404@proton.me>
2026-08-01 22:09:18 +0200

    added packages for store

--- Website/Website.Client/src/app/shared/store/session.store.ts
+++ Website/Website.Client/src/app/shared/store/session.store.ts
@@ -1,12 +1,27 @@
-// import {User} from "../../business/user/models/model";
-// import {signalStore} from '@ngrx/signals';
-//
-// type UserState = {
-//     data: User | null;
-// }
-//
-// const initialState: UserState = {
-//     data: null,
-// }
-//
-// export const SessionStore = signalStore
+import {User} from "../../business/user/models/model";
+import {signalStore, withComputed, withState} from '@ngrx/signals';
+import {computed} from "@angular/core";
+
+type SessionState = {
+    user: User | null;
+    isInitialized: boolean;
+    isLoading: boolean;
+    error: string | null;
+};
+
+const initialState: SessionState = {
+    user: null,
+    isInitialized: false,
+    isLoading: false,
+    error: null,
+}
+
+export const SessionStore = signalStore(
+    {providedIn: 'root'},
+    withState(initialState),
+
+    withComputed(({user, isInitialized}) => ({
+        isAuthenticated: computed(() => user() !== null),
+        isReady: computed(() => isInitialized()),
+    })),
+);
